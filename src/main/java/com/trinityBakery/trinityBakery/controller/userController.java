894f06f6ffbd7e16f7526829f855f34678956d43
dkyaorui <dkyaorui@163.com>
2018-07-24 01:03:45 +0800

    user

--- src/main/java/com/trinityBakery/trinityBakery/controller/userController.java
+++ src/main/java/com/trinityBakery/trinityBakery/controller/userController.java
@@ -4,13 +4,11 @@ package com.trinityBakery.trinityBakery.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.trinityBakery.trinityBakery.dao.goodRepository;
 import com.trinityBakery.trinityBakery.model.good;
@@ -21,17 +19,17 @@ import com.trinityBakery.trinityBakery.model.good;
 public class userController {
 	@Autowired
 	private goodRepository goodrepository;
-    
-    @GetMapping("shopping")
-    public String shopping() {
+
+
+
+    @RequestMapping(value = "/shopping",method = RequestMethod.GET)
+    public String shoppingxianshi(Map<String, Object> map) {
+        List<good> list = new ArrayList<good>();
+        list = goodrepository.findAll();
+        map.put("production", list);
         return "shopping";
     }
-    @PostMapping("shopping")
-    public List<good> shoppingxianshi() {
-    	List<good> list = new ArrayList<good>();
-    	list = goodrepository.findAll();
-    	return list;
-    }
+
     
     @RequestMapping("shoppingcart")
     public String shoppingcart() {
@@ -43,8 +41,10 @@ public class userController {
         return "user-order";
     }
     
-    @RequestMapping("user-order-detail")
-    public String user_order_detail() {
+    @RequestMapping("/user-order-detail/{id}")
+    public String user_order_detail(@PathVariable("id") Integer id) {
+        //通过id 查询订单详情
+        System.out.print(id);
         return "user-order-detail";
     }
     
@@ -53,8 +53,10 @@ public class userController {
         return "user-refund";
     }
     
-    @RequestMapping("user-refund-detail")
-    public String user_refund_detail() {
+    @RequestMapping("user-refund-detail/{id}")
+    public String user_refund_detail(@PathVariable("id") Integer id) {
+        //通过id 查询退款单详情
+        System.out.print(id);
         return "user-refund-detail";
     }
 
