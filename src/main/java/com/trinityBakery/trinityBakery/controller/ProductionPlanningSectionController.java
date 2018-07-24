201c9b22bfd84bbe59ab9c072481c4e9afb75570
dkyaorui <dkyaorui@163.com>
2018-07-24 17:44:46 +0800

    生产计划科

--- src/main/java/com/trinityBakery/trinityBakery/controller/ProductionPlanningSectionController.java
+++ src/main/java/com/trinityBakery/trinityBakery/controller/ProductionPlanningSectionController.java
@@ -1,6 +1,7 @@
 package com.trinityBakery.trinityBakery.controller;
 
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,34 @@ public class ProductionPlanningSectionController {
     
     @RequestMapping("pps-plan")
     public String pps_plan() {
+        //显示计划列表
         return "pps-plan";
+    }
+    @RequestMapping("/create-plan")
+    public String createPlan(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String num = request.getParameter("num");
+        String date = request.getParameter("date");
+        //创建生产计划
+
+        return "redirect:/pps-plan";
+    }
+    @RequestMapping("/change-plan")
+    public String changePlan(HttpServletRequest request){
+        String name = request.getParameter("c-name");
+        String num = request.getParameter("c-num");
+        String date = request.getParameter("c-date");
+        String id = request.getParameter("p-id");
+        //修改生产计划
+
+        return "redirect:/pps-plan";
+    }
+
+    @RequestMapping("/del-plan/{id}")
+    public String delPlan(@PathVariable("id") Integer id){
+        System.out.print(id);
+        //删除编号为id的plan
+        return "redirect:/pps-plan";
     }
     
     @RequestMapping("pps-refund")
